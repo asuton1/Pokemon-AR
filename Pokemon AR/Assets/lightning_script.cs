b0b25f91d6b3676624d8d980bd2180f839c8af46
Anita Suton <suton.anita@gmail.com>
2020-03-07 19:47:12 +0100

    Pokemon AR

---
+++ Pokemon AR/Assets/lightning_script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightning_script : MonoBehaviour
+{
+    public GameObject lightning, lightning1;
+   
+
+   public void LightningOn()
+    {
+        lightning1.SetActive(true);
+        lightning.SetActive(true);
+      
+    }
+
+    public void LightningOff()
+    {
+       lightning1.SetActive(false);
+        lightning.SetActive(false);
+      
+    }
+
+
+}
+
